fffee9d7cf740d5862bec6f28561c9260e28de95
Yngve <yngve-heill@hotmail.com>
2024-02-15 17:37:53 +0100

    Checkpoint before testing larger changes

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,5 +1,16 @@
-billettArray= [];
-function nyBillett() {
+// script.js
+
+ticketArray = [];
+
+//Getting variables ready for further use
+let selectedMovie = '';
+let selectedAmount = 0;
+let selectedFirstName = '';
+let selectedLastName = '';
+let selectedPhoneNumber =12345678;
+let selectedEmail = '';
+
+function newTicket() {
     let movie = document.getElementById("movie").value;
     let amount = document.getElementById("amount").value;
     let firstName = document.getElementById("firstName").value;
@@ -7,14 +18,44 @@ function nyBillett() {
     let phone = document.getElementById("phone").value;
     let email = document.getElementById("email").value;
 
-    let output = document.getElementById("output")
+    let ticket = {
+        movie: movie,
+        amount: amount,
+        firstName: firstName,
+        lastName: lastName,
+        phone: phone,
+        email: email
+    };
+
+    ticketArray.push(ticket);
+
+    //
+    let output = document.getElementById("output");
     output.innerHTML = `
-        <p>Movie: ${movie}</p>
-        <p>Amount: ${amount}</p>
-        <p>First Name: ${firstName}</p>
-        <p>Last Name: ${lastName}</p>
-        <p>Phone: ${phone}</p>
-        <p>Email: ${email}</p>
+        <p>Movie: ${ticket.movie}</p>
+        <p>Amount: ${ticket.amount}</p>
+        <p>First Name: ${ticket.firstName}</p>
+        <p>Last Name: ${ticket.lastName}</p>
+        <p>Phone: ${ticket.phone}</p>
+        <p>Email: ${ticket.email}</p>
     `;
+}
+
+function chooseMovie() {
+    let selectElement = document.getElementById("movie");
+    selectedMovie = selectElement.options[selectElement.selectedIndex].text;
+    console.log("Selected Movie: " + selectedMovie);
+}
+
+function showSummary() {
+    // Check if a movie is selected before showing the summary
+    if (selectedMovie === "") {
+        alert('No movie chosen!')
+    }
+    /*
+    else if(selectedAmount === 0){
+        alert('No tickets chosen')
+    }
+    */
 
 }
